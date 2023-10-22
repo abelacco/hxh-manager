961cc0964a8d41928c4ae5d0113aa9b339b7227a
MatiiChoque <choquematia@gmail.com>
2023-10-22 17:35:05 -0300

    update style modal Citas

--- src/app/modules/appointment/page/appointment-page.component.ts
+++ src/app/modules/appointment/page/appointment-page.component.ts
@@ -55,43 +55,45 @@ export class AppointmentPageComponent {
 
     confirm1(appointment: Appointment) {
         console.log('Cita a confirmar:', appointment);
-        this.confirmationService.confirm({
-            key: 'cd' + appointment._id,
-            message: '¿Deseas aceptar o rechazar el pago?',
-            header: 'Confirmación',
-            icon: 'pi pi-exclamation-triangle',
-            accept: () => {
-                // Aquí es donde actualizamos el estado de la cita y luego llamamos al servicio para guardar el cambio
-                // Puedes modificar el estado según tu lógica. Aquí estoy asumiendo que el estado se cambia a "accepted"
-                appointment.status = 2;
-                this.updateAppoinment(appointment);
-                this.messageService.add({
-                    severity: 'info',
-                    summary: 'Confirmed',
-                    detail: 'Cita Aceptada',
-                });
-            },
-            reject: (type: ConfirmEventType) => {
-                switch (type) {
-                    case ConfirmEventType.REJECT:
-                        // Aquí puedes manejar el rechazo. Si es necesario, también puedes actualizar la cita según tu lógica.
-                        appointment.status = 2;
-                        this.messageService.add({
-                            severity: 'error',
-                            summary: 'Rejected',
-                            detail: 'Cita Rechazada',
-                        });
-                        break;
-                    case ConfirmEventType.CANCEL:
-                        this.messageService.add({
-                            severity: 'warn',
-                            summary: 'Cancelled',
-                            detail: 'You have cancelled',
-                        });
-                        break;
-                }
-            },
-        });
+        if (appointment.status === 1) {
+            this.confirmationService.confirm({
+                key: 'cd' + appointment._id,
+                message: '¿Deseas aceptar o rechazar el pago?',
+                header: 'Confirmación',
+                icon: 'pi pi-exclamation-triangle',
+                accept: () => {
+                    // Aquí es donde actualizamos el estado de la cita y luego llamamos al servicio para guardar el cambio
+                    // Puedes modificar el estado según tu lógica. Aquí estoy asumiendo que el estado se cambia a "accepted"
+                    appointment.status = 2;
+                    this.updateAppoinment(appointment);
+                    this.messageService.add({
+                        severity: 'info',
+                        summary: 'Confirmed',
+                        detail: 'Cita Aceptada',
+                    });
+                },
+                reject: (type: ConfirmEventType) => {
+                    switch (type) {
+                        case ConfirmEventType.REJECT:
+                            // Aquí puedes manejar el rechazo. Si es necesario, también puedes actualizar la cita según tu lógica.
+                            appointment.status = 2;
+                            this.messageService.add({
+                                severity: 'error',
+                                summary: 'Rejected',
+                                detail: 'Cita Rechazada',
+                            });
+                            break;
+                        case ConfirmEventType.CANCEL:
+                            this.messageService.add({
+                                severity: 'warn',
+                                summary: 'Cancelled',
+                                detail: 'You have cancelled',
+                            });
+                            break;
+                    }
+                },
+            });
+        }
         console.log('Cita a confirmar:1');
     }
 
@@ -116,5 +118,13 @@ export class AppointmentPageComponent {
             detail: 'Cita Rechazada',
         });
         // Cerrar el diálogo manualmente (si es necesario)
+    }
+
+    cursorAppointment(appointment: Appointment) {
+        if (appointment.status === 1) {
+            return { cursor: 'pointer' };
+        } else {
+            return { cursor: 'auto' };
+        }
     }
 }
