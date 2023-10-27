20db1cd4eda3b602b04086068761699ffa464848
MatiiChoque <choquematia@gmail.com>
2023-10-27 16:30:03 -0300

    update - incorporacion de voucher con zoom

--- src/app/modules/appointment/interface/appointment.ts
+++ src/app/modules/appointment/interface/appointment.ts
@@ -5,24 +5,24 @@ export interface Doctor {
     speciality: string;
     fee: number;
     __v: number;
-  }
+}
 
-  export interface Patient {
+export interface Patient {
     _id: string;
     name: string;
     phone: string;
     dni: string;
     __v: number;
-  }
-
+}
 
 export interface Appointment {
-  _id: string;
-  doctorId: Doctor;
-  patientId: Patient;
-  date: Date;
-  fee: number;
-  status: number;
-  code: string;
-  __v: number;
+    _id: string;
+    doctorId: Doctor;
+    patientId: Patient;
+    date: Date;
+    fee: number;
+    status: number;
+    code: string;
+    voucher: string;
+    __v: number;
 }
