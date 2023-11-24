d771a76926f5b93cdb8ec0594b713e322c0810b4
Abel Acuna <abel3121@gmail.com>
2023-11-23 20:09:37 -0500

    update pipe loicalDateTime

--- src/app/modules/pipes/local-date-time.pipe.ts
+++ src/app/modules/pipes/local-date-time.pipe.ts
@@ -1,20 +1,30 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import * as moment from 'moment';
 
 @Pipe({
   name: 'localDateTime'
 })
 export class LocalDateTimePipe implements PipeTransform {
 
-  transform(value: string): string {
+//   transform(value: string): string {
+//     if (!value) return '';
+
+//     const date = new Date(value);
+//     const timeZone = 'America/Lima'; // Zona horaria de Perú
+//     return new Intl.DateTimeFormat('es-PE', {
+//       dateStyle: 'medium',
+//       timeStyle: 'short',
+//       timeZone: timeZone
+//     }).format(date);
+//   }
+transform(value: string): string {
     if (!value) return '';
 
-    const date = new Date(value);
-    const timeZone = 'America/Lima'; // Zona horaria de Perú
-    return new Intl.DateTimeFormat('es-PE', {
-      dateStyle: 'medium',
-      timeStyle: 'short',
-      timeZone: timeZone
-    }).format(date);
+    // Asegúrate de que moment interpreta la fecha como UTC
+    const date = moment.utc(value);
+
+    // Formatea la fecha sin convertirla a otra zona horaria
+    return date.format('DD-MM-YYYY');
   }
 
 }
